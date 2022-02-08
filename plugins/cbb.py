8107c72c8bdff6393bdbb07cb037f5d1d669652e
꧁SHͥAFͣIͫ࿐⁰⁰⁷ <95597052+AkramSanghar@users.noreply.github.com>
2022-02-08 08:12:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ If you also want your own <a href='https://t.me/NRM_FILE_STORE_BOT'>File Store Bot</a> with your update channel and support group then contact the developer</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
